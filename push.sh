5fe571ab9e343e079e3c2f2b0032f4672dcae890
Akshitha Thaneti <akshi@Akshithas-MacBook-Air.local>
2026-05-05 12:37:59 +0530

    Final fixed version: IamConvert Bot

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git init
 git add .
-git commit -m "initial commit: iamconvert bot"
+git commit -m "Final fixed version: IamConvert Bot"
 git branch -M main
-git remote add origin https://github.com/theRishu/iamconvert.git
-git push -u origin main
+git remote set-url origin https://github.com/theRishu/iamconvert.git || git remote add origin https://github.com/theRishu/iamconvert.git
+git push -u origin main -f
